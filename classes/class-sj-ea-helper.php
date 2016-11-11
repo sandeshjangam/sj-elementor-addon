659a6c19622f11266b05728ad0a1a059c23d3b98
Sandesh <sandeshj@bsf.io>
2016-11-11 15:19:34 +0530

    Initial commit

---
+++ classes/class-sj-ea-helper.php
@@ -0,0 +1,11 @@
+<?php 
+if ( ! class_exists( 'SJVccHelper' ) ) {
+	
+	/**
+	* Responsible for setting up constants, classes and includes.
+	*
+	* @since 0.1
+	*/
+	final class SJVccHelper {
+	}
+}
---
+++ classes/class-sj-ea-model.php
@@ -0,0 +1,24 @@
+<?php 
+if ( ! class_exists( 'SJVccModel' ) ) {
+	
+	/**
+	* Responsible for include modules.
+	*
+	* @since 0.1
+	*/
+	final class SJVccModel {
+		/**
+		 * Initialize hooks.
+		 *
+		 * @since 0.1 
+		 * @return void
+		 */
+		static public function init() {
+			
+			
+		}
+
+	}
+
+	SJVccModel::init();
+}
