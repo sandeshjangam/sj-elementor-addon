da7185b017d13de6aedfb891d9ac125422e730b9
Dinesh Chouhan <dcmayankavi@gmail.com>
2017-10-26 11:25:07 +0530

    Update sj-elementor-addon.php

--- sj-elementor-addon.php
+++ sj-elementor-addon.php
@@ -7,4 +7,11 @@ Author: sandesh055
 Version: 0.1.0
 */
 
+/**
+ * If this file is called directly, abort.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit();
+}
+
 require_once 'classes/class-sj-ea-loader.php';
